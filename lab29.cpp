d603dfa6d148f7c0e94825aeb39f804c9ca2d526
dickxumamba <xuruoyu27@gmail.com>
2025-11-06 13:56:47 +0800

    updated

--- lab29.cpp
+++ lab29.cpp
@@ -6,6 +6,10 @@
 #include <list>
 using namespace std;
 
+// We simulate a single cable machine in a 24-hour gym.
+// Map key: machine name ("Cable Machine").
+// Map value: array of 3 lists of member names.
+
 int main() {
-    
+
 }
